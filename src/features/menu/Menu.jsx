3777586fcb6f286d0ea69bd92eb5c18f35a1feb7
manishkhulbe <manishkhulbe123@gmail.com>
2023-08-11 17:52:18 +0530

    tailwind section

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -2,9 +2,11 @@ import { useLoaderData } from 'react-router-dom';
 import {getMenu} from '../../services/apiRestaurant'
 import MenuItem from './MenuItem';
 
+
+//space-x-4 to crete space in child element
 function Menu() {
   const menu = useLoaderData()
-  return <ul>{
+  return <ul className='divide-y divide-stone-200 px-2'>{
     menu.map((pizza) => {
       <MenuItem pizza={ pizza} key={pizza.key} />
   })
